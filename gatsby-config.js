17a94bb8daf440e77808156a5f80d6fdc95de861
陆文杰 <luwenjiechn@qq.com>
2021-05-14 10:32:37 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
 module.exports = {
   siteMetadata: {
     title: "blog",
+    author: "vg-land",
+    description: `My site description...`,
+    siteUrl: `https://vg-land.github.io`,
+    social: [
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
   },
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        /*
+        - basePath defaults to `/`
+        */
+        basePath: `/blog`,
+        prismPreset: `dracula`,
+        // preset: `my-preset-name-here`,
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
